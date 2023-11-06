9adf076df438b41dc8dfed61ddd7c18806f43f4f
crabyk <honzaf83@gmail.com>
2023-11-06 19:02:54 +0100

    Rozpracovany zakladni layout

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Oracle.ManagedDataAccess.Client;
+using Oracle.ManagedDataAccess.Types;
 
 namespace BDAS2_Restaurace
 {
@@ -23,6 +25,11 @@ namespace BDAS2_Restaurace
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
