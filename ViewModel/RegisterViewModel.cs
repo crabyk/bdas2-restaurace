647de44b4cb0b6550e65cede68eb396b0761f240
Martin <bigibosik@gmail.com>
2023-12-08 23:06:45 +0100

    Registrace uzivatele jako zakaznika

--- ViewModel/RegisterViewModel.cs
+++ ViewModel/RegisterViewModel.cs
@@ -6,7 +6,29 @@ namespace BDAS2_Restaurace.ViewModel
 {
     public class RegisterViewModel : ViewModelBase<User, UserController>
     {
+        private Customer newCustomer;
+        private Address newAddress;
         private string newPassword;
+
+        public Customer NewCustomer
+        {
+            get { return newCustomer; }
+            set
+            {
+                newCustomer = value;
+                OnPropertyChanged(nameof(NewCustomer));
+            }
+        }
+
+        public Address NewAddress
+        {
+            get { return newAddress; }
+            set
+            {
+                newAddress = value;
+                OnPropertyChanged(nameof(NewAddress));
+            }
+        }
 
         public string NewPassword
         {
@@ -20,6 +42,8 @@ namespace BDAS2_Restaurace.ViewModel
 
         public RegisterViewModel() : base(new UserController())
         {
+            NewAddress = new Address();
+            NewCustomer = new Customer();
             User newUser = new User();
             newUser.Role = new RoleController().Get("2");
             SelectedItem = newUser;
@@ -28,7 +52,15 @@ namespace BDAS2_Restaurace.ViewModel
 
         protected override void CreateMethod(object obj)
         {
-            new UserController().Register(SelectedItem, NewPassword);
+            User newUser = new UserController().Register(SelectedItem, NewPassword);
+            NewCustomer.User = newUser;
+            NewCustomer.FirstName = newUser.FirstName;
+            NewCustomer.LastName = newUser.LastName;
+
+            Address newAddress = new AddressController().Add(NewAddress);
+            NewCustomer.Address = newAddress;
+
+            new CustomerController().Add(NewCustomer);
             NewPassword = string.Empty;
             // base.CreateMethod(obj);
             // TODO vratit se zpet na domovskou stranku
