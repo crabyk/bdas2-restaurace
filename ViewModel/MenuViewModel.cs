5c346f5017ef3575a3ed047c076b4f54c4a1d922
crabyk <honzaf83@gmail.com>
2023-12-08 17:58:05 +0100

    Základní vzhled tlačítek, textboxů a datepickeru

--- ViewModel/MenuViewModel.cs
+++ ViewModel/MenuViewModel.cs
@@ -1,72 +1,21 @@
-﻿using BDAS2_Restaurace.Model;
-using BDAS2_Restaurace.Router;
+﻿using BDAS2_Restaurace.Controller;
+using BDAS2_Restaurace.Model;
+using System.Collections.ObjectModel;
+using System.Linq;
 
 namespace BDAS2_Restaurace.ViewModel
 {
-    public class MenuViewModel : RouteNavigation
+    public class MenuViewModel : BindableBase
     {
-        private DrinkViewModel drinkViewModel;
+        public ObservableCollection<Item> Items { get; set; }
 
-        public DrinkViewModel DrinkViewModel => drinkViewModel;
-
-        private FoodViewModel foodViewModel;
-
-        public FoodViewModel FoodViewModel => foodViewModel;
-
-        private Drink selectedDrink;
-        public Drink SelectedDrink
+        public MenuViewModel()
         {
-            get { return DrinkViewModel.SelectedItem; }
-            set
-            {
-                selectedDrink = value;
-                OnPropertyChanged(nameof(SelectedDrink));
-            }
+            // potreba to pak predelat na asynchronni verzi
+            var food = new FoodController().GetAll();
+            var drinks = new DrinkController().GetAll();
 
+            Items = new ObservableCollection<Item>(drinks.Cast<Item>().Concat(food.Cast<Item>()));
         }
-        public MenuViewModel() : base()
-        {
-
-            this.Routes.Add(new Route<DrinkViewModel>("drinks"));
-            this.Routes.Add(new Route<FoodViewModel>("food"));
-            this.Routes.Add(new Route<TestMenuViewModel>("testmenu"));
-
-        }
-        /*
-		public MenuViewModel()
-		{
-			NavCommand = new MyICommand<string>(OnNav);
-		}
-
-		private FoodViewModel foodViewModel = new FoodViewModel();
-		private DrinkViewModel drinkViewModel = new DrinkViewModel();
-
-		private BindableBase _CurrentViewModel;
-
-		public BindableBase CurrentViewModel
-		{
-			get { return _CurrentViewModel; }
-			set { SetProperty(ref _CurrentViewModel, value); }
-		}
-
-		public MyICommand<string> NavCommand { get; private set; }
-
-		private void OnNav(string destination)
-		{
-
-			switch (destination)
-			{
-				case "food":
-					CurrentViewModel = foodViewModel;
-					break;
-				case "drinks":
-					CurrentViewModel = drinkViewModel;
-					break;
-				default:
-					CurrentViewModel = null;
-					break;
-			}
-		}
-		*/
     }
 }
